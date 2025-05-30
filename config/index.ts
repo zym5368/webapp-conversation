3420ee26a712f990e1156d7309aefed5dbb52cca
zym5368 <50917270+zym5368@users.noreply.github.com>
2025-05-30 19:31:34 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,11 +3,11 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
+  title: '光达客服代表',
+  description: '光大集团—光达制造',
   copyright: '',
   privacy_policy: '',
-  default_language: 'en',
+  default_language: 'zh-Hans',
 }
 
 export const isShowPrompt = false
